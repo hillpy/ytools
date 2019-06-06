0a47be0b4daa64b686841cc08521f2b988a97e57
shinn-lancelot <945226793@qq.com>
2019-06-06 11:50:45 +0800

    test: 1. 更新测试用例

--- test/is.test.js
+++ test/is.test.js
@@ -1,25 +1,25 @@
 import is from '../src/js/is'
 
 test('isSupportTouch', () => {
-  expect(is.isSupportTouch())
+  expect(is.isSupportTouch()).toBe(false)
 })
 
 test('isSupportLocalStorage', () => {
-  expect(is.isSupportLocalStorage())
+  expect(is.isSupportLocalStorage()).toBe(true)
 })
 
 test('isSupportCookie', () => {
-  expect(is.isSupportCookie())
+  expect(is.isSupportCookie()).toBe(true)
 })
 
 test('18888888888 is phone ', () => {
-  expect(is.isMobilePhone(18888888888))
+  expect(is.isMobilePhone(18888888888)).toBe(true)
 })
 
 test('6.6 is number ', () => {
-  expect(is.isNumber(6.6))
+  expect(is.isNumber(6.6)).toBe(true)
 })
 
 test('44 is integer ', () => {
-  expect(is.isInteger(44))
+  expect(is.isInteger(44)).toBe(true)
 })
--- test/number.test.js
+++ test/number.test.js
@@ -1,5 +1,9 @@
 import number from '../src/js/number'
 
 test('getRandomInteger in 1 ~ 10', () => {
-  expect(number.getRandomInteger(1, 10))
+  expect(number.getRandomInteger(1, 10)).toBeGreaterThanOrEqual(1)
+})
+
+test('getRandomInteger in 1 ~ 10', () => {
+  expect(number.getRandomInteger(1, 10)).toBeLessThanOrEqual(10)
 })
